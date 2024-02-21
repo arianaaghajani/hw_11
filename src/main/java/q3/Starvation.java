6658494a9c46066b6d1c6aaf6da611d682743bba
arianaaghajani <aghajaniariana@gmail.com>
2024-02-21 20:48:18 +0330

    add main

--- src/main/java/q3/Starvation.java
+++ src/main/java/q3/Starvation.java
@@ -5,6 +5,13 @@ import java.util.concurrent.locks.ReentrantLock;
 public class Starvation {
     static ReentrantLock lock =new ReentrantLock(true);
 
+    public static void main(String[] args) {
+        for (int i = 0; i <= 5 ; i++) {
+            Thread thread= new Thread(new Worker(),"thread" + i);
+            thread.start();
+        }
+    }
+
 
     static class Worker implements Runnable {
         @Override
