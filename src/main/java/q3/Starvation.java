3db25fb770b6935bde64251d138f0305217492c3
arianaaghajani <aghajaniariana@gmail.com>
2024-02-21 20:47:20 +0330

    Worker implements Runnable

--- src/main/java/q3/Starvation.java
+++ src/main/java/q3/Starvation.java
@@ -1,4 +1,22 @@
 package q3;
 
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Starvation {
+    static ReentrantLock lock =new ReentrantLock(true);
+
+
+    static class Worker implements Runnable {
+        @Override
+        public void run() {
+            while (true){
+                try {
+                    lock.lock();
+                    System.out.println(Thread.currentThread().getName() + " the lock");
+                }finally {
+                    lock.unlock();
+                }
+            }
+        }
+    }
 }
